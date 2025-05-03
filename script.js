3a08840276bdf5272f3a0503d988f7e70ea7e1ef
Amirhossein1359 <amirnejad768@gmail.com>
2025-05-03 15:53:19 +0330

    Delete script.js

--- script.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener('DOMContentLoaded', function() {
-    const buttons = document.querySelectorAll('.product-card button');
-
-    buttons.forEach(function(button) {
-        button.addEventListener('click', function() {
-            alert('محصول به سبد خرید اضافه شد!');
-        });
-    });
-});
